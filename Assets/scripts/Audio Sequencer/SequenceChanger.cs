1b146d4bf95764be5227a14d192e52f48ca12676
shadowfax6894 <anniekelly6894@gmail.com>
2016-11-28 18:45:14 -0700

    first commit

---
+++ Assets/scripts/Audio Sequencer/SequenceChanger.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SequenceChanger : MonoBehaviour {
+
+    public Sequencer seq;
+    public string button;
+    public bool toggle;
+    public int index;
+    private MeshRenderer col;
+    public Material mat1;
+    public Material mat2;
+    public Material mat3;
+
+    // Use this for initialization
+    void Start () {
+        col = GetComponent<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        seq.sequence[index] = toggle;
+
+        if(Input.GetKeyDown(button))
+        {
+            toggle = !toggle;
+        }
+        if (toggle)
+        {
+            col.material = mat1;
+        }
+        else
+        {
+            col.material = mat2;
+        }
+
+        if (index == this.transform.parent.GetComponent<Sequencer>()._currentStep-1){
+            col.material = mat3;
+        }
+    }
+}
---
+++ Assets/scripts/audioVisualizer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class audioVisualizer : MonoBehaviour {
+
+    public GameObject prefab;
+    public int numberOfObjects = 20;
+    public float radius = 5f;
+    public GameObject[] cubes;
+    public int moveX;
+    public int moveY;
+    public int moveZ;
+
+    void Start() {
+        for (int i = 0; i < numberOfObjects; i++){
+            float angle = i * Mathf.PI * 2 / numberOfObjects;
+            Vector3 pos = new Vector3 (Mathf.Cos (angle), 0, Mathf.Sin (angle)) * radius;
+            pos = pos + new Vector3 (moveX, moveY, moveZ);
+            Instantiate (prefab, pos, Quaternion.identity);
+        }
+        cubes = GameObject.FindGameObjectsWithTag ("cubes");
+    }
+    void Update(){
+        float[] spectrum = AudioListener.GetSpectrumData (1024, 0, FFTWindow.Hamming);
+        for(int i = 0; i<numberOfObjects; i++){
+            Vector3 previousScale = cubes[i].transform.localScale;
+            previousScale.y = Mathf.Lerp (previousScale.y, spectrum[i] * 40, Time.deltaTime * 30);
+            cubes[i].transform.localScale = previousScale;
+
+        }
+    }
+}
